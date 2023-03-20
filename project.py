c1eba585871644a73798e17e948f5f93fbdec981
JacketPotato35 <127979504+JacketPotato35@users.noreply.github.com>
2023-03-20 22:15:01 +0000

    cant forget the message

--- project.py
+++ project.py
@@ -14,8 +14,8 @@ display = pygame.display.set_mode((screen_width, screen_height))
 draw_surface=pygame.Surface((2000,2000))
 pygame.display.set_caption('Hello World!')
 
-print("this message is to see if i can update the git version")
-
+print("this message is to see if i can update the git version, so question is what am i doing")
+print("bruh what am i doing")
 player = Player(screen_width, screen_height)
 player_group = pygame.sprite.Group()
 player_group.add(player)
